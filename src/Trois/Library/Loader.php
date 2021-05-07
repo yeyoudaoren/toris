1a2521f104138c9a44bea06176daeffc2e6eb02e
yeyoudaoren <yeyoudaoren@gmail.com>
2021-05-07 17:04:24 +0800

    first commit

---
+++ src/Trois/Library/Loader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Create by dings
+ * User:dings
+ * Email:yeyoudaoren@gmail.com
+ * Date 2021/05/07
+ * Time:16:59
+ */
+namespace Toris\Library;
+
+class Loader {
+  public function index(){
+    echo "Hello Loader";
+  }
+}
